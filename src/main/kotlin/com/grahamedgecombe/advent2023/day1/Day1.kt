9a0b18e1b0443dbdd144e9356f78b00e06cfdda3
Graham Edgecombe <gpe@grahamedgecombe.com>
2023-12-01 08:53:21 +0000

    Add day 1 part 2 solution

--- src/main/kotlin/com/grahamedgecombe/advent2023/day1/Day1.kt
+++ src/main/kotlin/com/grahamedgecombe/advent2023/day1/Day1.kt
@@ -1,8 +1,14 @@
 package com.grahamedgecombe.advent2023.day1
 
 import com.grahamedgecombe.advent2023.Puzzle
+import com.grahamedgecombe.advent2023.UnsolvableException
 
 object Day1 : Puzzle<List<String>>(1) {
+    private val WORDS = listOf(
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        "1", "2", "3", "4", "5", "6", "7", "8", "9"
+    )
+
     override fun parse(input: Sequence<String>): List<String> {
         return input.toList()
     }
@@ -13,5 +19,17 @@ object Day1 : Puzzle<List<String>>(1) {
             val last = line.last(Char::isDigit).digitToInt()
             first * 10 + last
         }
+    }
+
+    override fun solvePart2(input: List<String>): Int {
+        return input.sumOf { line ->
+            val (_, first) = line.findAnyOf(WORDS) ?: throw UnsolvableException()
+            val (_, last) = line.findLastAnyOf(WORDS) ?: throw UnsolvableException()
+            wordToInt(first) * 10 + wordToInt(last)
+        }
+    }
+
+    private fun wordToInt(word: String): Int {
+        return word.toIntOrNull() ?: (WORDS.indexOf(word) + 1)
     }
 }
--- src/test/kotlin/com/grahamedgecombe/advent2023/day1/Day1Test.kt
+++ src/test/kotlin/com/grahamedgecombe/advent2023/day1/Day1Test.kt
@@ -10,12 +10,27 @@ class Day1Test {
         assertEquals(54940, Day1.solvePart1(PROD_INPUT))
     }
 
+    @Test
+    fun testPart2() {
+        assertEquals(281, Day1.solvePart2(TEST_INPUT_2))
+        assertEquals(54208, Day1.solvePart2(PROD_INPUT))
+    }
+
     private companion object {
         private val TEST_INPUT_1 = Day1.parse("""
             1abc2
             pqr3stu8vwx
             a1b2c3d4e5f
             treb7uchet
+        """.trimIndent())
+        private val TEST_INPUT_2 = Day1.parse("""
+            two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen
         """.trimIndent())
         private val PROD_INPUT = Day1.parse()
     }
